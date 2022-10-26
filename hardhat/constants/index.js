a6981397e86172d1eaeb4a21b6a7b36a205e79af
Levente Csoke <lc@gerhadrt.io>
2022-10-26 10:07:54 +0200

    Initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x19E7495FeD7438f2e56DD03A894749ACDfcdD8af";
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
